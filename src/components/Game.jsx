cb90ee80aeb8d4b0cedc841102bb6f0f835ccad7
Vaibhav Mangesh Lokhande <Vaibhav.Lokhande@freecharge.com>
2022-11-17 23:27:04 +0530

    Added Game componet

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Game() {
-    return <div role="mainTitle">hello</div>
+    const squares = Array(9).fill(null)
+
+    function renderTile(id) {
+        return <input type="button" value={squares[id]} className="box" onClick={() => selectTile(id)} />
+    }
+
+    function selectTile(id) {
+        squares[id] = 1;
+    }
+    return (
+        <div role="mainTitle" className={"mainTitle"}>
+            <div>
+                {renderTile(1)}
+                {renderTile(2)}
+                {renderTile(3)}
+            </div>
+            <div>
+                {renderTile(4)}
+                {renderTile(5)}
+                {renderTile(6)}
+            </div>
+            <div>
+                {renderTile(7)}
+                {renderTile(8)}
+                {renderTile(9)}
+            </div>
+        </div>)
 }
+
 
 export default Game;
